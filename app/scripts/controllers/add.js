ff7fad2bb1309e020c722bf490d72d58c197e8b6
Doru Chiulan <doru.chiulan@gmail.com>
2016-03-08 02:08:00 +0200

    Invoice generation now works.

--- app/scripts/controllers/add.js
+++ app/scripts/controllers/add.js
@@ -8,52 +8,87 @@
  * Controller of the facturiSswApp
  */
 angular.module('facturiSswApp')
-  .controller('AddCtrl', function ($clients, $scope, FoundationApi, ModalFactory, $timeout) {
+  .controller('AddCtrl', function ($facturi, $clients, $scope, FoundationApi, ModalFactory, $timeout) {
     var vm = this;
+
+    vm.language = 'ro';
+    vm.int8n = {
+      ro: {
+        date: "Data",
+        invoice: "Factura Fiscala",
+        series: "Serie",
+        number: "Numar",
+        seller: "Furnizor",
+        buyer: "Cumparator",
+        name: "Denumirea firmei",
+        cif: "Codul unic de identificare",
+        orc: "Numar ordin ORC",
+        address: "Sediul",
+        city: "Oras",
+        region: "Judet",
+        country: "Tara",
+        bank: "Banca",
+        bankAccount: "IBAN",
+        total: "TOTAL",
+        addProduct: "ADAUGA PRODUS",
+        description: "Descriere",
+        product: "Produs",
+        value: "Valoare",
+        amount: "Suma",
+        productName: "Nume Produs",
+        saveInvoice: "SALVEAZA FACTURA"
+      }
+    };
 
     vm.clients = $clients('list');
     vm.client = {
-      banca: [{ nume: '', cont: ''}]
+      name: '',
+      cif: '',
+      orc: '',
+      address: '',
+      city: '',
+      region: '',
+      country: '',
+      banks: [{
+        name: '',
+        account: ''}]
     };
 
     vm.f = {
       series: "",
       number: "",
+      buyer: "",
       date: "",
-      currency: "",
+      currency: "RON",
       seller: {
         name: "SANDD SOFT WORKS SRL",
         cif: "33535396",
-        orc: "J/25/235/2014",
+        orc: "J25/235/2014",
         address: "Smardan 26, bl. S4E, ap. 4",
         city: "Dr. Tr. Severin",
         region: "Mehedinti",
         country: "Romania",
-        bank: "ING Bank",
-        bankAccount: "RO71INGB0000999901641743"
+        bank: "ING Bank N.V. Amsterdam - Bucharest",
+        bankAccount: "RO95INGB0000999904642494"
       },
       products: [
-        { amount: 10, title: "Dezvoltare software la comanda", desc: "Conform contractului 01.01.2016" },
-        { amount: 25, title: "Dezvoltare software la comanda", desc: "Conform contractului 01.01.2016" }
       ]
     };
+
     if (vm.f.date == "") {
       vm.f.date = new Date();
     }
 
     vm.total = calculateTotal(vm.f.products);
 
-    function calculateTotal(products) {
-      if (products === undefined) return 0;
-      var s = 0;
-      for (var i=0; i < products.length; i++) {
-        s += products[i].amount;
-      }
-      return s;
-    }
-
     vm.addProduct = function () {
       vm.f.products.push({amount: "", title: "", desc: ""});
+    };
+
+    vm.removeProduct = function(idx) {
+      if(vm.f.products != undefined) {
+        vm.f.products.splice(idx, 1);
+      }
     };
 
     vm.openClientModal = function (client) {
@@ -65,13 +100,15 @@ angular.module('facturiSswApp')
           templateUrl: 'views/_/add-client-modal.html',
           contentScope: {
             addAccount: function (){
-              if (client.banca.length > 2) {
+              if (client.banks.length > 2) {
                 return;
               }
-              client.banca.push({});
+              client.banks.push({});
             },
             removeAccount: function (index){
-              client.banca.splice(index, 1);
+              if (index > 0) {
+                client.banks.splice(index, 1);
+              }
             },
             int8n: vm.int8n,
             client: client,
@@ -103,13 +140,26 @@ angular.module('facturiSswApp')
             },
             add: function() {
               closeClientModal(modal);
-              vm.openClientModal({banca: [{}]})
+              vm.openClientModal({banks: [{}]})
             },
             clients: vm.clients
           }
         });
         modal.activate();
       };
+
+    vm.recalculateTotal = function() {
+      vm.total = calculateTotal(vm.f.products);
+    };
+
+    // TODO: add some form validation to ensure we have minimum data required
+    vm.saveFactura = function () {
+      if (vm.f._id !== undefined) {
+        $facturi.update(vm.f);
+      } else {
+        $facturi.push(vm.f);
+      }
+    };
 
     function closeClientModal(modal) {
       modal.deactivate();
@@ -118,38 +168,19 @@ angular.module('facturiSswApp')
       }, 1000);
     }
 
+    function calculateTotal(products) {
+      if (products === undefined) return 0;
+      var s = 0;
+      for (var i=0; i < products.length; i++) {
+        s += products[i].amount;
+      }
+      return s;
+    }
+
     $scope.$watch('vm.client', function(client) {
-      if (client.nume === undefined && client.banca[0].nume === '') {
+      if (client.name === undefined && client.banks[0].nume === '') {
         return;
       }
       vm.f.buyer = client._id;
     });
-
-    vm.language = 'ro';
-    vm.int8n = {
-      ro: {
-        date: "Data",
-        invoice: "Factura Fiscala",
-        series: "Serie",
-        number: "Numar",
-        seller: "Furnizor",
-        buyer: "Cumparator",
-        name: "Denumirea firmei",
-        cif: "Codul unic de identificare",
-        orc: "Numar ordin ORC",
-        address: "Sediul",
-        city: "Oras",
-        region: "Judet",
-        country: "Tara",
-        bank: "Banca",
-        bankAccount: "IBAN",
-        total: "TOTAL",
-        addProduct: "ADAUGA PRODUS",
-        description: "Descriere",
-        product: "Produs",
-        value: "Valoare",
-        amount: "Suma",
-        productName: "Nume Produs"
-      }
-    }
   });
---
+++ app/scripts/services/facturi.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name facturiSswApp.facturi
+ * @description
+ * # facturi
+ * Service in the facturiSswApp.
+ */
+angular.module('facturiSswApp')
+  .service('$facturi', ['$http', function ($http) {
+    var url = "http://localhost:8080/facturi/";
+    var facturi = [];
+
+    this.init = function(callback) {
+      if(facturi.length > 0) return;
+      return $http.get(url).success(function(data) {
+        facturi = data;
+        if (callback) {
+          callback(data);
+        }
+      });
+    };
+
+    this.list = function() {
+      return facturi;
+    };
+
+    this.factura = function(index) {
+      return facturi[index];
+    };
+
+    this.getById = function(id) {
+      for(var i = 0, len = facturi.length; i < len; i++) {
+        if(facturi[i]._id == id) {
+          return facturi[i];
+        }
+      }
+      return undefined;
+    };
+
+    this.push = function(factura) {
+      $http.post(url, factura).success(function(data) {
+        if(data._id) {
+          facturi.push(data);
+        }
+      });
+    };
+
+    this.update = function(factura) {
+      $http.post(url, factura).success(function(data) {
+        if(data._id) {
+          facturi.push(data);
+        }
+      });
+    };
+  }]);
